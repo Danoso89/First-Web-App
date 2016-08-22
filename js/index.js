3c297559d5c3099abc39b48f0b66abb7db0cf101
Dani <dangonrei@gmail.com>
2016-08-22 20:17:18 +0200

    Implemented the async calls to the api with promises

--- js/index.js
+++ js/index.js
@@ -2,18 +2,6 @@
 
   var baseUrl = 'https://api.github.com/users/';
 
-  var callback = function(data) {
-    $(".info-container").slideDown();
-    
-    // Save relevant info in local storage
-    localStorage.setItem(data.login,
-        JSON.stringify({name: data.name, user: data.login,
-        repos: data.public_repos}));
-    $("#name").text(data.name);
-    $("#user-name").text(data.login);
-    $("#repos").text(data.public_repos);
-  };
-
   $().ready(function() {
 
     // Pressing 'Enter' work as pressing the button
@@ -25,16 +13,26 @@
 
     $('#btn-get').click(function() {
       var userName = $("input").val();
-      if (userName) {
+      if (!!userName) {
         console.log('llama a la api con userName = ' + userName);
-        ModuloAPI.getData(baseUrl + userName, callback);
+        ModuloAPI.getData(baseUrl + userName).done(data => {
+          console.log('Working with promises :)')
+          $(".info-container").slideDown();
+          
+          // Save relevant info in local storage
+          localStorage.setItem(data.login,
+              JSON.stringify({name: data.name, user: data.login,
+              repos: data.public_repos}));
+          $("#name").text(data.name);
+          $("#user-name").text(data.login);
+          $("#repos").text(data.public_repos);
+        });
+
       } else {
         $(".info-container").slideUp();
       }
     });
 
   });
-  
-
 
 })()
--- js/modulo.js
+++ js/modulo.js
@@ -1,6 +1,8 @@
 var ModuloAPI = (function($) {
 
   var api = {};
+
+  /* With callback
   api.getData = function(url, callback) {
     $.getJSON(url, function(data) {
       if (callback) {
@@ -8,6 +10,20 @@ var ModuloAPI = (function($) {
       }
     })
   };
+  */
+
+  // With promises
+    // using the promise returned byy getJSON [usign this one]
+  api.getData = url => $.getJSON(url);
+
+    // using custom promise [coded to practice on $.Deferred()]
+  api.getDataPromise = function(url) {
+    console.log('A custom promise is created at modulo.js');
+    var deferred = $.Deferred();
+    $.getJSON(url, data => deferred.resolve(data));
+    console.log('promise attached to the JSON, ready to return it');
+    return deferred.promise();
+  }
 
   return api;
 })(jQuery)
